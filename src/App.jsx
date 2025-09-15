c9d436ff1c28fd4529faaa21d73f30ff1dc5369d
tanishijanweja <tanishi.janweja@gmail.com>
2025-09-15 23:27:49 +0530

    redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Blog app </h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
+
 
 export default App;
